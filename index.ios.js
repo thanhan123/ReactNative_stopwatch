be71c8c94b2c277ba8dec544f3f2e83412a397fc
thanhan123 <thanhan.uit@gmail.com>
2017-03-18 16:11:03 +0700

    finish stop watch react native app

--- index.ios.js
+++ index.ios.js
@@ -8,32 +8,149 @@ import {
   Text,
   View,
   AppRegistry,
+  TouchableHighlight,
   StyleSheet
 } from 'react-native';
 
+var formatTime = require('minutes-seconds-milliseconds');
+
 var StopWatch = React.createClass({
+  getInitialState(){
+    return{
+      timeElapsed: null,
+      running: false,
+      startTime: null,
+      laps: []
+    }
+  },
   render() {
-    return <View>
-      <Text>
-        00:00.00
-      </Text>
-      {this.startStopButton()}
-      {this.lapButton()}
+    return <View style={styles.container}>
+      <View style={[styles.header]}>
+        <View style={[styles.timerWrapper]}>
+          <Text style={styles.timer}>
+            {formatTime(this.state.timeElapsed)}
+          </Text>
+        </View>
+        <View style={[styles.buttonWrapper]}>
+          {this.startStopButton()}
+          {this.lapButton()}
+        </View>
+      </View>
+
+      <View style={[styles.footer]}>
+        {this.laps()}
+      </View>
     </View>
+  },
+  laps(){
+    return this.state.laps.map((time, index)=>{
+      return <View style={styles.lap}>
+        <Text style={styles.lapText}>
+          Lap #{index + 1}
+        </Text>
+        <Text style={styles.lapText}>
+          {formatTime(time)}
+        </Text>
+      </View>
+    });
   },
   startStopButton() {
-    return <View>
+    var style = this.state.running ? styles.stopButton : styles.startButton;
+
+    return <TouchableHighlight
+      underlayColor="gray"
+      onPress={this.handleStartPress}
+      style={[styles.button, style]}
+      >
       <Text>
-        Start
+        {this.state.running ? 'Stop' : 'Start'}
       </Text>
-    </View>
+    </TouchableHighlight>
   },
   lapButton() {
-    return <View>
+    return <TouchableHighlight
+      style={styles.button}
+      underlayColor='gray'
+      onPress={this.handleLapPress}
+      >
       <Text>
         Lap
       </Text>
-    </View>
+    </TouchableHighlight>
+  },
+  handleStartPress(){
+    if (this.state.running) {
+      clearInterval(this.interval);
+      this.setState({running: false});
+      return;
+    }
+
+    this.setState({
+      startTime: new Date()
+    });
+
+    this.interval = setInterval(()=> {
+      this.setState({
+        timeElapsed: new Date() - this.state.startTime,
+        running: true
+      });
+    }, 30);
+  },
+  handleLapPress(){
+    var lap = this.state.timeElapsed;
+
+    this.setState({
+      startTime: new Date(),
+      laps: this.state.laps.concat([lap])
+    });
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch'
+  },
+  header: {
+    flex: 1
+  },
+  footer: {
+    flex: 1
+  },
+  timerWrapper: {
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonWrapper: {
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  timer: {
+    fontSize: 60
+  },
+  button: {
+    borderWidth: 1,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  startButton: {
+    borderColor: '#00CC00'
+  },
+  stopButton: {
+    borderColor: '#CC0000'
+  },
+  lap: {
+    justifyContent: 'space-around',
+    flexDirection: 'row'
+  },
+  lapText: {
+    fontSize: 30
   }
 });
 
